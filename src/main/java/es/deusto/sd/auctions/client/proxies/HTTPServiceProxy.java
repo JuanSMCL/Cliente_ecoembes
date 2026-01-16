3fdf63fe47ad81658e50b4788bd7129391446c46
Ibai Velada <ibai.velada@opendeusto.es>
2026-01-16 16:33:48 +0100

    loggin creado

--- src/main/java/es/deusto/sd/auctions/client/proxies/HTTPServiceProxy.java
+++ src/main/java/es/deusto/sd/auctions/client/proxies/HTTPServiceProxy.java
@@ -1,8 +1,15 @@
 package es.deusto.sd.auctions.client.proxies;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import es.deusto.sd.auctions.client.data.ContenedorRango;
+import es.deusto.sd.auctions.client.data.Usuario;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -14,10 +21,12 @@ public class HTTPServiceProxy implements IServiceProxy {
     private final HttpClient httpClient;
     private final ObjectMapper objectMapper;
 
-    public HTTPServiceProxy(HttpClient httpClient, ObjectMapper objectMapper) {
-        this.httpClient = httpClient;
-        this.objectMapper = objectMapper;
+    public HTTPServiceProxy() {
+        this.httpClient = HttpClient.newHttpClient();
+        this.objectMapper = new ObjectMapper();
     }
+
+
 
     @Override
     public ContenedorRango consultarEstadoContenedor(Long id, LocalDate fIni, LocalDate fFin, long token) {
@@ -43,4 +52,29 @@ public class HTTPServiceProxy implements IServiceProxy {
         }
         return null;
     }
+
+    @Override
+    public String logear(Usuario usuario)  {
+        try{
+            String json = objectMapper.writeValueAsString(usuario);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create((BASE_URL + "/login")))
+                    .POST(HttpRequest.BodyPublishers.ofString(json))
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return switch (response.statusCode()) {
+                case 200 -> response.body(); // Successful login, returns token
+                case 401 -> throw new RuntimeException("Unauthorized: Invalid credentials");
+                default -> throw new RuntimeException("Login failed with status code: " + response.statusCode());
+            };
+        } catch (IOException | InterruptedException e){
+            throw new RuntimeException("Ha habido un error al procesar la peticion");
+        }
+
+
+    }
+
+
 }
--- src/main/java/es/deusto/sd/auctions/client/proxies/IServiceProxy.java
+++ src/main/java/es/deusto/sd/auctions/client/proxies/IServiceProxy.java
@@ -1,9 +1,16 @@
 package es.deusto.sd.auctions.client.proxies;
 
+import java.io.IOException;
 import java.time.LocalDate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import es.deusto.sd.auctions.client.data.ContenedorRango;
+import es.deusto.sd.auctions.client.data.Usuario;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface IServiceProxy {
     ContenedorRango consultarEstadoContenedor(Long id, LocalDate fIni, LocalDate fFin, long token);
-
+    String logear(Usuario usuario) throws IOException, InterruptedException;
 }
